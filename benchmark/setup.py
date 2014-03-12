66b67cfb9ba8344dc3901ed59baeec1ffc22fdbe
Álvaro Villalba <alvaro.villalba@bsc.es>
2014-03-12 17:43:31 +0100

    wip

--- benchmark/setup.py
+++ benchmark/setup.py
@@ -39,7 +39,7 @@ def put_topology(config):
     for i in range(num_sos):
         new_initial_streams, new_prev_streams = put_so(config, prev_streams)
         initial_streams += new_initial_streams
-        prev_streams = new_prev_streams
+        prev_streams = new_prev_streams + prev_streams
     return initial_streams
 
 
@@ -58,8 +58,8 @@ def put_initial_so(config):
         json_stream = get_stream(config)
         if json_stream is None:
             continue
-        json_so["streams"][str(i)] = json_stream
-        streams += [str(i)]
+        json_so["streams"]["stream" + str(i)] = json_stream
+        streams += ["stream" + str(i)]
 
     response, content = request('', 'POST', json.dumps(json_so), config)
     if int(response['status']) >= 300:
@@ -91,15 +91,80 @@ def get_stream(config):
 
 
 def put_so(config, prev_streams):
-    return
+    initial_stream_ids = []
+    stream_ids = []
+    initial_streams = []
+    streams = []
+    num_streams = round(eval(config['TOPOLOGIES']['Streams']))
+    if num_streams < 0:
+        num_streams = 0
+
+    json_file = open('./jsons/so.json')
+    json_so = json.load(json_file)
+    json_file.close()
+    # Get streams
+    for i in range(num_streams):
+        json_stream = get_stream(config)
+        if json_stream is None:
+            continue
+        json_so["streams"]["stream" + str(i)] = json_stream
+        initial_stream_ids += ["stream" + str(i)]
+        stream_ids += ["stream" + str(i)]
+
+    # Get groups
+    json_so["groups"] = get_groups(config, prev_streams)
+
+    # Get cstreams
+    json_so["cstreams"] = get_cstreams(config, json_so["groups"])
+
+    response, content = request('', 'POST', json.dumps(json_so), config)
+
+    if int(response['status']) >= 300:
+        print(content + '\n')
+        return []
+    json_content = json.loads(content)
+    so_id = json_content['id']
+    for stream_id in stream_ids:
+        streams += [[so_id, stream_id]]
+
+    for initial_stream_id in initial_stream_ids:
+        initial_streams += [[so_id, initial_stream_id]]
+
+    return initial_streams, streams
 
 
 def get_groups(config, prev_streams):
-    return
+    num_groups = round(eval(config['TOPOLOGIES']['Groups']))
+    member_distribution = config['TOPOLOGIES']['MemberDistribution']
+    groups = {}
+    if num_groups < 1:
+        return None
+    if member_distribution == 'deterministic':
+        return get_groups_det(config, prev_streams)
+
+    for i in range(num_groups):
+        not_found = True
+        while not_found:
+            sel_stream = round(eval(config['TOPOLOGIES']['MemberDistribution']))
+            if sel_stream < 0 or sel_stream >= len(prev_streams):
+                continue
+
+            groups["group" + i]["soIds"] = prev_streams[sel_stream][0]
+            groups["group" + i]["stream"] = prev_streams[sel_stream][1]
+            not_found = False
+    return groups
 
 
-def get_group(config, streams):
-    return
+def get_groups_det(config, prev_streams):
+    num_groups = round(eval(config['TOPOLOGIES']['Groups']))
+    groups = {}
+    if num_groups < 1:
+        return None
+    for i in range(num_groups):
+        groups["group" + i]["soIds"] = prev_streams[i % (len(prev_streams))][0]
+        groups["group" + i]["stream"] = prev_streams[i % (len(prev_streams))][1]
+
+    return groups
 
 
 def get_cstreams(config, groups):
