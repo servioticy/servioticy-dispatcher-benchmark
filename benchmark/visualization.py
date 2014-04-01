f4695850459f55f9c5a92309e0951531e684780e
Álvaro Villalba Navarro <vn.alvaro@gmail.com>
2014-04-01 20:22:20 +0200

    WIP

--- benchmark/visualization.py
+++ benchmark/visualization.py
@@ -8,30 +8,31 @@ import os
 
 
 def main():
-    graphs = []
+    graphs = {}
     if os.path.isdir(sys.argv[1]):
         for file in os.listdir(sys.argv[1]):
-            graphs.append(nx.read_gml(sys.argv[1] + '/' + file))
+            graphs[file] = nx.read_gml(sys.argv[1] + '/' + file)
     elif os.path.isfile(sys.argv[1]):
-        graphs.append(nx.read_gml(sys.argv[1]))
+        graphs[sys.argv[1]] = nx.read_gml(sys.argv[1])
 
     if len(sys.argv) > 2:
         label = sys.argv[2]
         if len(sys.argv) > 3:
             label += ":" + sys.argv[3]
-        for graph in graphs:
-            for i in range(len(graph.node)):
-                current_label = graph.node[i]['label']
+        for graph_key in graphs.keys():
+            for i in range(len(graphs[graph_key].node)):
+                current_label = graphs[graph_key].node[i]['label']
                 if current_label == label:
-                    p.figure(label)
-                    nx.draw_networkx(nx.bfs_tree(graph, i), with_labels=False)
+                    G = nx.bfs_tree(graphs[graph_key], i)
+                    for node in G.node:
+                        G.add_edges_from(nx.bfs_edges(graphs[graph_key], node))
+                    p.figure(graph_key + " - " + label)
+                    nx.draw_networkx(G, with_labels=False)
 
     else:
-        i = 0
-        for graph in graphs:
-            p.figure(i)
-            nx.draw_networkx(graph, with_labels=False)
-            ++i
+        for graph_key in graphs.keys():
+            p.figure(graph_key)
+            nx.draw_networkx(graphs[graph_key], with_labels=False)
 
     p.show()
     return
