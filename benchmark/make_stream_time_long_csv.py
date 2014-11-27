ea68fcd08862ec469dc004d6efbcf8b7b00ba095
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2014-11-27 19:49:44 +0100

    WIP

--- benchmark/make_stream_time_long_csv.py
+++ benchmark/make_stream_time_long_csv.py
@@ -2,6 +2,8 @@ import sys
 import csv
 
 def main():
+    lastUpdate = 0
+    waits = []
     with open(sys.argv[2], 'r', newline='') as f:
 
         reader1 = csv.reader(f, delimiter=',', quotechar='"')
@@ -14,11 +16,15 @@ def main():
                     continue
                 new_row = [0,0]
                 i = 5
-                while i + 3 < len(row):
+                while i + 3 < len(row) and row[i+3] != '':
                     new_row[0] = row[i+3]
                     new_row[1] = int(row[i+3]) - int(row[i])
                     writer.writerow(new_row)
                     i = i+3
+                if lastUpdate != 0:
+                    waits.append(int(row[1]) - int(lastUpdate))
+                lastUpdate = row[1]
+            print(str(1000/(sum(waits)/len(waits))) + ' req/s\n')
 
     return
 
