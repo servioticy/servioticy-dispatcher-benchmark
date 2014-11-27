be82ef0e43cf3ba3e56e412a0b3b750cfe67fb1b
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2014-11-27 19:38:38 +0100

    WIP

--- benchmark/make_exec_time_long_csv.py
+++ benchmark/make_exec_time_long_csv.py
@@ -2,6 +2,10 @@ import sys
 import csv
 
 def main():
+    lastUpdate = 0
+    lastNStreams = 0
+    waits = []
+    exec_times = []
     with open(sys.argv[1], 'r', newline='') as f:
 
         reader1 = csv.reader(f, delimiter=',', quotechar='"')
@@ -10,8 +14,24 @@ def main():
             writer = csv.writer(f2, delimiter=',',
                                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for row in reader1:
-                new_row = [row[4], int(row[2])-int(row[5])]
-                writer.writerow(new_row)
+                if lastNStreams == 0:
+                    lastNStreams = row[4]
+                if lastNStreams == row[4]:
+                    exec_times.append(int(row[2])-int(row[5]))
+                else:
+                    new_row = [str(int(lastNStreams)-1), sum(exec_times)/len(exec_times)]
+                    writer.writerow(new_row)
+                    exec_times = []
+                    lastNStreams = row[4]
+
+                if lastUpdate != 0:
+                    waits.append(int(row[1]) - int(lastUpdate))
+                lastUpdate = row[1]
+
+            new_row = [lastNStreams, sum(exec_times)/len(exec_times)]
+            writer.writerow(new_row)
+            print(str(1000/(sum(waits)/len(waits))) + ' req/s\n')
+
 
     return
 
